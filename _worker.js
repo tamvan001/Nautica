a9be3e37f2fa29ae202bcc9f3b53d4c3dc6cc040
tamvan001 <58065425+tamvan001@users.noreply.github.com>
2024-12-21 22:42:21 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,12 +1,12 @@
 import { connect } from "cloudflare:sockets";
 
 // Variables
-const rootDomain = "yasnaya.my.id"; // Ganti dengan domain utama kalian
-const serviceName = "cf"; // Ganti dengan nama workers kalian
-const apiKey = "WIEnSiopi49OkiYyj0SrALwHriioGwhAy8Vy62IW"; // Ganti dengan Global API key kalian (https://dash.cloudflare.com/profile/api-tokens)
-const apiEmail = "miftahulfebri35@gmail.com"; // Ganti dengan email yang kalian gunakan
-const accountID = "5f41862f4cc305cffde93d292b544be3"; // Ganti dengan Account ID kalian (https://dash.cloudflare.com -> Klik domain yang kalian gunakan)
-const zoneID = "e21ad2f50005007aff3826ada4f9f196"; // Ganti dengan Zone ID kalian (https://dash.cloudflare.com -> Klik domain yang kalian gunakan)
+const rootDomain = "febriganteng.us.kg"; // Ganti dengan domain utama kalian
+const serviceName = "cf-ganteng"; // Ganti dengan nama workers kalian
+const apiKey = "Y7x23pxmiV7Xg0TcnXktGb_Tn8KrBqpyakDtKUcx"; // Ganti dengan Global API key kalian (https://dash.cloudflare.com/profile/api-tokens)
+const apiEmail = "miftahulfebri36@gmail.com"; // Ganti dengan email yang kalian gunakan
+const accountID = "819de5d9251200b181216bcc20d0e7a6"; // Ganti dengan Account ID kalian (https://dash.cloudflare.com -> Klik domain yang kalian gunakan)
+const zoneID = "e597e61b8c5f1ec6c4df9af557765020"; // Ganti dengan Zone ID kalian (https://dash.cloudflare.com -> Klik domain yang kalian gunakan)
 let isApiReady = false;
 let proxyIP = "";
 let cachedProxyList = [];
